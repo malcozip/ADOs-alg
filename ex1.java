b52ff21d8883f091c15636bbb2b5abe394973166
malco <fellipemalcolm@gmail.com>
2022-11-16 01:37:31 -0300

    conclusão exercicio 1 da ADO1

--- ex1.java
+++ ex1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class ex1 {
 public static void main(String[] args){
-System.out.println("hello world");
+Scanner ler = new Scanner(System.in);
+int valor1=0;
+int valor2=0;
+int soma=0;
+
+
+   System.out.println("Digite o primeiro valor:");
+    valor1 = ler.nextInt();
+ System.out.println("Digite o segundo valor:");
+    valor2 = ler.nextInt();
+    soma = valor1 + valor2;
+ System.out.println("A soma de "+valor1+ "+" +valor2+ " é " +soma);
 }
 }
